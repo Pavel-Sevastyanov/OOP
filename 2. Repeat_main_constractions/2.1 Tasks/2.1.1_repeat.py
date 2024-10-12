0bcfcbb6457aff2b811bb8f3f487169ee0f8f3fb
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-10-12 17:34:25 +0400

    complited

--- 2. Repeat_main_constractions/2.1 Tasks/2.1.1_repeat.py
+++ 2. Repeat_main_constractions/2.1 Tasks/2.1.1_repeat.py
@@ -1,1 +1,9 @@
 side = int(input())
+matrix = [[1 for j in range(side)] for i in range(side)]
+
+for i in range(side):
+    for j in range(side):
+        matrix[i][j] = min(i + 1, j + 1, side - i, side - j)
+
+for row in matrix:
+    print(*row)
