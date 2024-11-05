23d9ddaa9fd4a906b8a04213f625d19b4ed79b62
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-11-05 20:57:42 +0400

    complited

---
+++ 6. Protocols/6.5 Protocol of context managers. Part 2/6.5.6_task.py
@@ -0,0 +1,15 @@
+import sys
+
+class UpperPrint:
+    def __init__(self):
+        self.print = print
+
+    def upper_out(self, text):
+        return self.standart_out(text.upper())
+
+    def __enter__(self):
+        self.standart_out = sys.stdout.write
+        sys.stdout.write = self.upper_out
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        sys.stdout.write = self.standart_out
