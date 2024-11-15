ce8c698333947f989869024d44b414b067d47376
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-11-15 19:48:13 +0400

    complited

---
+++ 7. Inheritance and polymorphism/7.4 Inheritance.Part 4/7.4.3_task.py
@@ -0,0 +1,6 @@
+from collections import UserDict
+
+class TwoWayDict(UserDict):
+    def __setitem__(self, key, value):
+        self.data.__setitem__(key, value)
+        self.data.__setitem__(value, key)
