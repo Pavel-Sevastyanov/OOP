30f41ead28d48cc09935ac21626bfe2fdaad328b
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-11-21 20:24:38 +0400

    complited

--- 7. Inheritance and polymorphism/7.7 Polymorphism/7.7.3_task.py
+++ 7. Inheritance and polymorphism/7.7 Polymorphism/7.7.3_task.py
@@ -9,8 +9,33 @@ class Paragraph(ABC):
 
     def add(self, string):
         for word in string.split():
-            if self.length_row + len(word) - 1 <= self.length:
+            if self.length_row + len(word) <= self.length:
                 self.row += word + ' '
                 self.length_row += len(word) + 1
             else:
                 self.rows.append(self.row.rstrip())
+                self.row = word + ' '
+                self.length_row = len(word) + 1
+
+    @abstractmethod
+    def end(self):
+        pass
+
+class LeftParagraph(Paragraph):
+    def end(self):
+        self.rows.append(self.row.rstrip())
+        for row in self.rows:
+            print(row)
+        self.rows = []
+        self.row = ''
+        self.length_row = 0
+
+class RightParagraph(Paragraph):
+    def end(self):
+        self.rows.append(self.row.rstrip())
+        for row in self.rows:
+            space = self.length - len(row)
+            print(f'{space * " "}{row}')
+        self.rows = []
+        self.row = ''
+        self.length_row = 0      
