e7aa914ee63fe55f58a5ffb9f30ccfa56bcc2095
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-11-25 21:01:03 +0400

    complited

---
+++ 8. Additional features/8.4 Decorators. Part 1/8.4.4_task.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+class returns:
+    def __init__(self, datatype):
+        self.datatype = datatype
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if not isinstance(result, self.datatype):
+                raise TypeError
+            return result
+        return wrapper
