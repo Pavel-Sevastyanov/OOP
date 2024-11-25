75f91aefd7cc116bac2892a416666c04b3b5e322
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-11-25 19:58:58 +0400

    complited

---
+++ 8. Additional features/8.3 Bit flags, Flag class/8.3.2_task.py
@@ -0,0 +1,21 @@
+from enum import Flag
+
+
+class MovieGenres(Flag):
+    ACTION = 1
+    COMEDY = 2
+    DRAMA = 4
+    FANTASY = 8
+    HORROR = 16
+
+
+class Movie:
+    def __init__(self, name, genres):
+        self.name = name
+        self.genres = genres
+
+    def in_genre(self, genre):
+        return genre in self.genres
+
+    def __str__(self):
+        return self.name
