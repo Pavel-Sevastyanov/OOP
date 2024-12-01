fb6a28ea47cc5e2ba4e4ef8a8a6e1b2f4ec92164
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2024-12-01 18:50:03 +0400

    complited

--- 8. Additional features/8.7 Mixins/8.7.4_task.py
+++ 8. Additional features/8.7 Mixins/8.7.4_task.py
@@ -1,13 +1,8 @@
 class ToStringMixin:
     def __repr__(self):
-        format_dict = {}
-        count = 0
-        for k, v in self.__dict__.items():
-            format_dict[k] = v
-            count += 1
-            if count == 6:
-                break
-        dotes = '' if len(self.__dict__ ) <= 6 else ', ...'
-        return f"{self.__class__.__name__}({format_dict}{dotes})"
+        format_dict = [f'{repr(k)}: {repr(v)}' for k, v in self.__dict__.items()]
+        if len(self.__dict__) > 6:
+            format_dict = format_dict[:6] + ['...']
+        return f"{self.__class__.__name__}(\u007b{', '.join(format_dict)}\u007d)"
 
     
