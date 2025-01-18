f1c1a2509be64db96e86b32e2b580895825afd3a
Pavel Sevastyanov <sevastyanovit27@mail.ru>
2025-01-18 09:16:37 +0400

    complited

---
+++ 9. Tasks/9.1 Tasks/9.1.6_task.py
@@ -0,0 +1,17 @@
+class HighScoreTable:
+    def __init__(self, max_records):
+        self.max_records = max_records
+        self._scores = []
+
+    @property
+    def scores(self):
+        return self._scores
+
+    def update(self, record):
+        self._scores.append(record)
+        self._scores.sort(reverse=True)
+        if len(self._scores) > self.max_records:
+            self._scores = self._scores[:self.max_records]
+
+    def reset(self):
+        self._scores.clear()
